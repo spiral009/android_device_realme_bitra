480ceffb745d7a3724129ef3d605d6eff8d14221
spiral009 <po99h6@gmail.com>
2024-08-16 04:22:56 +0000

    fixes

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ $(call inherit-product, device/realme/sm8250-common/common.mk)
 $(call inherit-product, vendor/realme/bitra/bitra-vendor.mk)
 
 # API
-BOARD_API_LEVEL := 30
+#BOARD_API_LEVEL := 30
 BOARD_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
 PRODUCT_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
 
